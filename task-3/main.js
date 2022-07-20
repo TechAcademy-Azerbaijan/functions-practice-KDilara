36d872d7f90dc800ef1f1019b760f6dc0bb9c7a6
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-20 23:43:02 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,6 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function revrs(...arr){
+    for(i=arr.length-1; i>=0; i--){
+        console.log(arr[i])
+        }
+}
+revrs(92846,96841,33582,25998)
