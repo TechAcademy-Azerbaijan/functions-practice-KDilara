252d8a4fe65afb4b74ffe688e0935dded199d08c
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-20 23:40:38 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,15 +1,21 @@
-function sade(num){
-  let ans=" "
-    for(i=2; i<num; i++){
-        if(num%i==0){
-            ans="No"
-            break
-        }else{
-            ans="Yes"
-        }
-    }
-    console.log(ans)
-
+function sum(x) {
+  if (x >= 100) {
+    a = parseInt(x / 100);
+    b = parseInt(x / 10) % 10;
+    c = x % 10;
+    num = a * 100 + parseInt(b / 2) * 30 + c * 2 + (b % 2) * 20;
+  } else if (x < 100) {
+    a = 0;
+    b = parseInt(x / 10) % 10;
+    c = x % 10;
+    num = parseInt(b / 2) * 30 + c * 2 + (b % 2) * 20;
+  } else if (x < 10) {
+    a = 0;
+    b = 0;
+    c = x;
+    num = c * 2;
+  }
+  console.log(num);
 }
 
-sade(12)
+sum(123);
